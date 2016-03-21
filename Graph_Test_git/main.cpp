27f5903565da65963b97789559043a91f6a4ecd1
麦小明 <Michael@maixiaomingdeMacBook-Air.local>
2016-03-22 07:52:10 +0800

    Initial Commit

---
+++ Graph_Test_git/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Graph_Test_git
+//
+//  Created by 麦小明 on 16/3/22.
+//  Copyright (c) 2016年 麦小明. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
